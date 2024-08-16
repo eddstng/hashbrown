7abb147d09ec37044e5416afefab957cf054f6fa
Edmond Lee <eddstng@gmail.com>
2024-08-16 13:29:41 -0700

    update Sha1Hasher finalize fn and add test for Sha1Hasher

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use sha1::{Digest, Sha1};
+use sha1::{ Digest, Sha1 };
 
 trait Hasher {
     fn update(&mut self, data: &[u8]);
-    fn finalize() -> String;
+    fn finalize(self) -> String;
 }
 
 struct Sha1Hasher {
@@ -23,18 +23,41 @@ impl Hasher for Sha1Hasher {
         self.state.update(data);
     }
 
-    fn finalize() -> String {
-        return "".to_string();
+    fn finalize(self) -> String {
+        // https://docs.rs/sha1/latest/sha1/trait.Digest.html#tymethod.finalize
+        let result: sha1::digest::generic_array::GenericArray<
+            u8,
+            sha1::digest::typenum::UInt<
+                sha1::digest::typenum::UInt<
+                    sha1::digest::typenum::UInt<
+                        sha1::digest::typenum::UInt<
+                            sha1::digest::typenum::UInt<
+                                sha1::digest::typenum::UTerm,
+                                sha1::digest::consts::B1
+                            >,
+                            sha1::digest::consts::B0
+                        >,
+                        sha1::digest::consts::B1
+                    >,
+                    sha1::digest::consts::B0
+                >,
+                sha1::digest::consts::B0
+            >
+        > = self.state.finalize();
+        println!("{:x}", result);
+        return format!("{:x}", result);
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn test_sha_1_hasher() {
-//         let mut hasher = Sha1Hasher::new();
-//         hasher.update(b"helloworld");
-//     }
-// }
+    #[test]
+    fn test_sha_1_hasher() {
+        let mut hasher = Sha1Hasher::new();
+        hasher.update(b"helloworld");
+        let result = hasher.finalize();
+        assert_eq!(result, "6adfb183a4a2c94a2f92dab5ade762a47889a5a1")
+    }
+}
